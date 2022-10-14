109090b9fd2b12a228e674f54702223ae42c1ea0
GDIV <gdavisiv@gmail.com>
2022-10-14 08:51:23 -0400

    Creating Client Section

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import { clients } from "../constants";
+import styles from "../style";
 
-const Clients = () => {
-  return (
+const Clients = () => (
     <div>Clients</div>
-  )
-}
+)
 
-export default Clients
+export default Clients;
